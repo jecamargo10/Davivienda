d90414384e4bf92125535f7f89ef0b7abc16f6c9
jecamargo10 <javier_22ca@hotmail.com>
2016-08-09 00:05:04 -0500

    Añadir funcionalidad web con extraccion de URL

---
+++ app/src/main/java/com/example/javie/davivienda/LoadListener.java
@@ -0,0 +1,7 @@
+package com.example.javie.davivienda;
+
+/**
+ * Created by javie on 08/08/2016.
+ */
+public class LoadListener {
+}
--- app/src/main/java/com/example/javie/davivienda/MainActivity.java
+++ app/src/main/java/com/example/javie/davivienda/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.javie.davivienda;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +22,73 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Uri uri = Uri.parse("http://stggrupobolivar.taleo.net");
-        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-        startActivity(intent);
 
+        final    WebView myWebView = (WebView) findViewById(R.id.webView);
+            WebSettings webSettings = myWebView.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+
+            myWebView.setWebViewClient(new WebViewClient());
+        myWebView.getSettings().setBuiltInZoomControls(true);
+        myWebView.getSettings().setSupportZoom(true);
+        myWebView.setInitialScale(135);
+        myWebView.loadUrl("http://stggrupobolivar.taleo.net");
+
+
+        myWebView.addJavascriptInterface(new MyJavaScriptInterface(this), "HtmlViewer");
+        myWebView.setWebViewClient(new WebViewClient()
+                                 {
+                                     @Override
+                                     public void onPageFinished(WebView view, String url)
+                                     {
+                                         myWebView.loadUrl("javascript:window.HtmlViewer.showHTML" +
+                                                 "('&lt;html&gt;'+document.getElementsByTagName('html')[0].innerHTML+'&lt;/html&gt;');");
+                                     }
+                                 }
+        );
+        myWebView.loadUrl("http://stggrupobolivar.taleo.net");
+    }
+    class MyJavaScriptInterface
+    {
+        private Context ctx;
+        MyJavaScriptInterface(Context ctx)
+        {
+            this.ctx = ctx;
+        }
+        @JavascriptInterface
+
+        public void showHTML(String html)
+        {
+            Log.e("result",html);
+
+        }
     }
 }
+
+
+
+    /**
+     *      myWebView.setWebViewClient(new WebViewClient());
+     myWebView.addJavascriptInterface(new LoadListener(), "HTMLOUT");
+     myWebView.getSettings().setBuiltInZoomControls(true);
+     myWebView.getSettings().setSupportZoom(true);
+     myWebView.setInitialScale(135);
+     myWebView.loadUrl("http://stggrupobolivar.taleo.net");
+     * myWebView.loadUrl("javascript:window.HTMLOUT.showHTML(http://stggrupobolivar.taleo.net);");
+        myWebView.setWebViewClient(new WebViewClient() {
+
+            public void onPageFinished(WebView view, String url) {
+
+                Log.e("result",view.get);
+            }
+        });
+
+    }
+    class LoadListener{
+        @JavascriptInterface
+
+        public void showHTML(String html)
+        {
+            Log.e("result",html);
+        }
+    }**/
+
